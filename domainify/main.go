607b326ca81c0919cbf702537fe51f81e7bbf2de
rita <kajirita2002@gmail.com>
2021-05-17 18:45:25 +0900

    added domainify

---
+++ domainify/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+	"unicode"
+)
+
+var tlds = []string{"com", "net"}
+
+const allowedChars = "abcdefghijklmnopqrstuvwxyz0123456789_-"
+
+func main() {
+	// シード値のセット
+	rand.Seed(time.Now().UTC().UnixNano())
+	// os.Stdinをラップ
+	s := bufio.NewScanner(os.Stdin)
+
+	for s.Scan() {
+		text := strings.ToLower(s.Text())
+		var newText []rune
+		for _, r := range text {
+			// 空白文字があった場合
+			if unicode.IsSpace(r) {
+				r = '-'
+			}
+			// allowedCharsに含まれている文字だけをnewTextに追加していく
+			if !strings.ContainsRune(allowedChars, r) {
+				continue
+			}
+			newText = append(newText, r)
+		}
+		fmt.Println(string(newText) + "." + tlds[rand.Intn(len(tlds))])
+	}
+}
